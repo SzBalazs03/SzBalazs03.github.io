45f822fb1bfb30bbc11bd26870f02b0fe8f33502
Szabó Balázs <szabobalazs03.21@gmail.com>
2022-03-17 20:24:06 +0100

    initializeCards function body added
    click eventListener to algorithm selector
    text values array stores card data

--- cards.js
+++ cards.js
@@ -1,3 +1,29 @@
+
+
+function initializeCards() {
+    algCard.changeData()
+
+    
+    let algSelect = document.getElementById("algorithm")
+    algSelect.addEventListener("click", function(){ changeValue()})
+
+}
+
+const textValues = [
+    ["A* search",
+    "is a best-first search, meaning that it is formulated in terms of weighted graphs: starting from a specific starting node of a graph, it aims to find a path to the given goal node having the smallest cost (least distance travelled, shortest time, etc.)."
+    
+],
+    ["Breadth-first search",
+    "is an algorithm for searching a tree data structure. It starts at the tree root and explores all nodes at the present depth prior to moving on to the nodes at the next depth level."
+],
+    ["",
+    ""
+]
+]
+
+function changeValue(){
+}
 class bootStrapCard {
     constructor(card, title, text) {
         this.card = card
@@ -33,3 +59,5 @@ class bootStrapCard {
 
    
 }
+
+const algCard = new bootStrapCard(document.getElementById("algCard"), textValues[0][0], textValues[0][1])
