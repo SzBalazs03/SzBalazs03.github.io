d320ca94e8e713f314afe4311a66f462df07e59f
Szabó Balázs <szabobalazs03.21@gmail.com>
2022-03-24 16:40:42 +0100

    added new text for the content of cards

--- cards.js
+++ cards.js
@@ -10,14 +10,16 @@ function initializeCards() {
 
 const textValues = [
     ["A* search",
-    "is a best-first search, meaning that it is formulated in terms of weighted graphs: starting from a specific starting node of a graph, it aims to find a path to the given goal node having the smallest cost (least distance travelled, shortest time, etc.)."
-    
+    "is a best-first search, meaning that it is formulated in terms of weighted graphs: starting from a specific starting node of a graph, it aims to find a path to the given goal node having the smallest cost (least distance travelled, shortest time, etc.).",
+    "https://en.wikipedia.org/wiki/A*_search_algorithm"
 ],
     ["Breadth-first search",
-    "is an algorithm for searching a tree data structure. It starts at the tree root and explores all nodes at the present depth prior to moving on to the nodes at the next depth level."
+    "is an algorithm for searching a tree data structure. It starts at the tree root and explores all nodes at the present depth prior to moving on to the nodes at the next depth level.",
+    "https://en.wikipedia.org/wiki/Breadth-first_search"
 ],
-    ["",
-    ""
+    ["Depth-first search",
+    "is an algorithm for traversing or searching tree or graph data structures. It starts at the root node and explores as far as possible along each branch before backtracking.",
+    "https://en.wikipedia.org/wiki/Depth-first_search"
 ]
 ]
 
