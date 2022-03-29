8f9bd86a5e07bbf64dd83e8fffb3c13e99d5bbe1
Szabó Balázs <szabobalazs03.21@gmail.com>
2022-03-29 20:16:33 +0200

    algTextValues changed to key:value array

--- cards.js
+++ cards.js
@@ -8,21 +8,6 @@ function initializeCards() {
 
 }
 
-const textValues = [
-    ["A* search",
-    "is a best-first search, meaning that it is formulated in terms of weighted graphs: starting from a specific starting node of a graph, it aims to find a path to the given goal node having the smallest cost (least distance travelled, shortest time, etc.).",
-    "https://en.wikipedia.org/wiki/A*_search_algorithm"
-],
-    ["Breadth-first search",
-    "is an algorithm for searching a tree data structure. It starts at the tree root and explores all nodes at the present depth prior to moving on to the nodes at the next depth level.",
-    "https://en.wikipedia.org/wiki/Breadth-first_search"
-],
-    ["Depth-first search",
-    "is an algorithm for traversing or searching tree or graph data structures. It starts at the root node and explores as far as possible along each branch before backtracking.",
-    "https://en.wikipedia.org/wiki/Depth-first_search"
-]
-]
-
 function changeValue(){
     let algSelect = document.getElementById("algorithm")
     switch (algSelect.value) {
@@ -82,6 +67,24 @@ class bootStrapCard {
 }
 
 const algCard = new bootStrapCard(document.getElementById("algCard"), textValues[0][0], textValues[0][1], textValues[0][2])const algTextValues = {
+const algTextValues = {
+    aStar: {
+        title : "A* search",
+        body: "is a best-first search, meaning that it is formulated in terms of weighted graphs: starting from a specific starting node of a graph, it aims to find a path to the given goal node having the smallest cost (least distance travelled, shortest time, etc.).",
+        href: "https://en.wikipedia.org/wiki/A*_search_algorithm"
+    },
+    bfs: {
+        title: "Breadth-first search",
+        body: "is an algorithm for searching a tree data structure. It starts at the tree root and explores all nodes at the present depth prior to moving on to the nodes at the next depth level.",
+        href: "https://en.wikipedia.org/wiki/Breadth-first_search"
+    },
+    dfs: {
+        title: "Depth-first search",
+        body: "is an algorithm for traversing or searching tree or graph data structures. It starts at the root node and explores as far as possible along each branch before backtracking.",
+        href: "https://en.wikipedia.org/wiki/Depth-first_search"
+    }
+}
+
 const mazeGenTextValues = {
     backTrack: {
         title: "Randomized depth-first search",
